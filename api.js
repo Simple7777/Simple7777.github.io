3fc6b7e7a4793f0567d3bfe31b880c051b1c93a9
Simple <98197295+Simple7777@users.noreply.github.com>
2023-01-09 00:11:46 -0300

    first commit

---
+++ api.js
@@ -0,0 +1,14 @@
+axios.get('https://api.orgdestiny.me/xyz/903339096074747935')
+  .then(function (response) {
+    // Aqui vc trata os dados, os dados retornados estarão dentro do parâmetro response
+    document.getElementById("avatar").src = 'https://cdn.discordapp.com/avatars/903339096074747935/' + response.data.user.avatar + '?size=1024'
+  })
+  .catch(function (error) {
+    // Caso dê algum erro você pode tratar aqui 
+    console.log(error);
+  })
+
+  let typed = new Typed('.nome', {
+    strings: ["Hi, I'm Simple, Welcome to my portfolio."],
+    typeSpeed: 37
+  });
