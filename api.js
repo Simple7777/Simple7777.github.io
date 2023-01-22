cda505d14f280f6e941390b27b8f481af80d9f9d
argondc <90789517+argondc@users.noreply.github.com>
2023-01-22 16:14:10 -0300

    fix: responsivity changes

--- api.js
+++ api.js
@@ -4,11 +4,12 @@ axios.get('https://api.orgdestiny.me/xyz/903339096074747935')
     document.getElementById("avatar").src = 'https://cdn.discordapp.com/avatars/903339096074747935/' + response.data.user.avatar + '?size=1024'
   })
   .catch(function (error) {
-    // Caso dê algum erro você pode tratar aqui 
+    // Caso dê algum erro você pode tratar aqui
     console.log(error);
   })
 
   let typed = new Typed('.nome', {
     strings: ["Hi, I'm Simple, Welcome to my portfolio."],
-    typeSpeed: 37
+    typeSpeed: 37,
+    showCursor: false
   });
